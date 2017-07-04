89a221431d7c77ee212f05f4fe63688a36fda3b6
DrXrayW <xray.wang@gmail.com>
2017-07-04 14:26:15 -0400

    Update CopyListwithRandomPointer01.java

--- src/xray/leetcode/linkList/CopyListwithRandomPointer01.java
+++ src/xray/leetcode/linkList/CopyListwithRandomPointer01.java
@@ -2,6 +2,9 @@ package xray.leetcode.linkList;
 
 public class CopyListwithRandomPointer01 {
 	/*
+	A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
 	 * the hard point is to find the new created node for the new list
 	 * 
 	 * no hashmap for this one to save spaces
